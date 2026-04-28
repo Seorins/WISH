582b22522b0af79c27f1dc41ea6ab2ea751b66f7
장영철 <1231yw1103@naver.com>
2026-04-29 08:54:20 +0900

    [S14P31E103-86] FE/feat: Top 체조 디자인 초안 구성

--- frontend/apps/game/src/phaser.ts
+++ frontend/apps/game/src/phaser.ts
@@ -6,6 +6,7 @@ import { ArtColoringSelectScene } from './scenes/themes/art/coloring/ArtColoring
 import { ArtFreeDrawingScene } from './scenes/themes/art/free-drawing/ArtFreeDrawingScene'
 import { ArtSelectScene } from './scenes/themes/art/select/ArtSelectScene'
 import { TaekwondoSelectScene } from './scenes/themes/taekwondo/select/TaekwondoSelectScene'
+import { GymnasticsTopScene } from './scenes/themes/gymnastics/play/GymnasticsPlayScene'
 import { GymnasticsSelectScene } from './scenes/themes/gymnastics/select/GymnasticsSelectScene'
 import { VillageScene } from './scenes/village/VillageScene'
 
@@ -31,6 +32,7 @@ export function createGame(parent: HTMLElement): Phaser.Game {
       ArtColoringScene,
       TaekwondoSelectScene,
       GymnasticsSelectScene,
+      GymnasticsTopScene,
     ],
     scale: {
       mode: Phaser.Scale.RESIZE,
