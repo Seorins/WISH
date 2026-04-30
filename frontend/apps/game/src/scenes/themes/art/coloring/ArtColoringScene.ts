9d0627cadad304bd841145e503696812e69382d6
박서린 <tjfls295@naver.com>
2026-04-30 15:32:22 +0900

    [S14P31E103-106] FE/chore: 미술 끊어짐 현상 해결

--- frontend/apps/game/src/scenes/themes/art/coloring/ArtColoringScene.ts
+++ frontend/apps/game/src/scenes/themes/art/coloring/ArtColoringScene.ts
@@ -19,7 +19,7 @@ const DELETE_BUTTON_SIZE = { width: 344, height: 336 }
 const ART_ROOM_RETURN_SPAWN = { xRatio: 0.5, yRatio: 0.76 }
 const FILLABLE_WHITE_THRESHOLD = 245
 const HAND_FILL_COOLDOWN_MS = 320
-const HAND_FILL_HOLD_MS = 720
+const HAND_FILL_HOLD_MS = 900
 const HAND_FILL_STILL_RADIUS = 58
 const EXIT_CONFIRM_DEPTH = 60
 type ColoringTool = 'brush' | 'eraser'
@@ -75,7 +75,7 @@ export class ArtColoringScene extends Phaser.Scene {
   private coloringTexture!: Phaser.GameObjects.RenderTexture
   private paletteSelection!: Phaser.GameObjects.Arc
   private cameraPreview: ArtCameraPreview | null = null
-  private brushCursor!: Phaser.GameObjects.Image
+  private brushCursor: Phaser.GameObjects.Image | null = null
   private handFillGauge: Phaser.GameObjects.Graphics | null = null
   private toolButtons: Partial<Record<ColoringTool, Phaser.GameObjects.Image>> = {}
   private toolButtonFrames: Partial<Record<ColoringTool, Phaser.GameObjects.Arc>> = {}
@@ -236,6 +236,7 @@ export class ArtColoringScene extends Phaser.Scene {
     this.strokeCount = 0
     this.handTrackingDisposed = false
     this.currentTool = 'brush'
+    this.brushCursor = null
     this.cameraPreview = null
     this.toolButtons = {}
     this.toolButtonFrames = {}
@@ -303,6 +304,7 @@ export class ArtColoringScene extends Phaser.Scene {
       this.cameraPreview?.destroy()
       this.cameraPreview = null
       this.stopHandTracking()
+      this.brushCursor = null
     })
 
     this.cameras.main.fadeIn(220, 0, 0, 0)
@@ -727,20 +729,21 @@ export class ArtColoringScene extends Phaser.Scene {
   }
 
   private updateBrushCursorTexture() {
-    if (!this.brushCursor) {
+    const brushCursor = this.brushCursor
+    if (!brushCursor || !brushCursor.scene || !brushCursor.active) {
       return
     }
 
     if (this.currentTool === 'eraser') {
-      this.brushCursor.setTexture('art-ui-eraser')
-      this.brushCursor.setDisplaySize(54, 54)
-      this.brushCursor.setOrigin(0.5, 0.5)
+      brushCursor.setTexture('art-ui-eraser')
+      brushCursor.setDisplaySize(54, 54)
+      brushCursor.setOrigin(0.5, 0.5)
       return
     }
 
-    this.brushCursor.setTexture('art-ui-brush')
-    this.brushCursor.setDisplaySize(48, 48)
-    this.brushCursor.setOrigin(0.18, 0.88)
+    brushCursor.setTexture('art-ui-brush')
+    brushCursor.setDisplaySize(48, 48)
+    brushCursor.setOrigin(0.18, 0.88)
   }
 
   private startHandColoringMode(delegate: 'GPU' | 'CPU' = 'GPU') {
